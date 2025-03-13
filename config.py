65230a6b36b0cdbbe42f20d483eba596d8649794
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-03-13 22:48:36 +0600

    Api Updates

--- config.py
+++ config.py
@@ -2,3 +2,6 @@
 API_ID = "282xx710"  # Replace with your API ID Get From my.telegram.org
 API_HASH = "7fc5b3ddghsdxxxb86481ab5eca3"  # Replace with your API Hash Get From my.telegram.org
 BOT_TOKEN = "78249889xxxxffergipu88wmA9u1tOtq8qddzhyL0"  # Replace with your Bot Token Get From @BotFather 
+TEXT_MODEL= "deepseek-r1-distill-qwen-32b"
+GROQ_API_URL= "https://api.groq.com/openai/v1/chat/completions"
+GROQ_API_KEY= "gsk_iV0BIeqCEBW5PZVrmykeJ55sitUMK"
