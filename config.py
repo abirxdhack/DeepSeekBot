6c808ea9a6db4adf7c750cde5282c5b235669f19
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-04-30 06:52:27 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Replace these with your actual API details
-API_ID = "282xx710"  # Replace with your API ID Get From my.telegram.org
-API_HASH = "7fc5b3ddghsdxxxb86481ab5eca3"  # Replace with your API Hash Get From my.telegram.org
-BOT_TOKEN = "78249889xxxxffergipu88wmA9u1tOtq8qddzhyL0"  # Replace with your Bot Token Get From @BotFather 
-TEXT_MODEL= "deepseek-r1-distill-qwen-32b"
+API_ID = "YOUR_API_ID"  # Replace with your API ID Get From my.telegram.org
+API_HASH = "YOUR_API_HASH"  # Replace with your API Hash Get From my.telegram.org
+BOT_TOKEN = "YOUR_BOT_TOKEN"  # Replace with your Bot Token Get From @BotFather 
+TEXT_MODEL= "deepseek-r1-distill-llama-70b" #Don't Change This Model 
 GROQ_API_URL= "https://api.groq.com/openai/v1/chat/completions"
-GROQ_API_KEY= "gsk_iV0BIeqCEBW5PZVrmykeJ55sitUMK"
+GROQ_API_KEY= "YOUR_GROQ_API_KEY"
